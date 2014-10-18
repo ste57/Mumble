94905a854677416c93e1613a52028b756b98109a
Stephen Sowole <tunsow@hotmail.com>
2014-10-18 14:02:06 +0100

    First Commit
    - Added resources folder with mumble designs and presentation
    - Added initial iOS files

---
+++ iOS/Mumble/Mumble/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mumble
+//
+//  Created by Stephen Sowole on 18/10/2014.
+//  Copyright (c) 2014 Stephen Sowole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
